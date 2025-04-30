6eb7b438a91fa7dca089dbe1247505bead9c53a1
Baizidmdashadzzaman <baizid.md.ashadzzaman@gmail.com>
2025-04-30 09:53:54 +0600

    added package

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\SubscriptionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/subscribe', [SubscriptionController::class, 'showForm'])->name('subscribe.form');
+Route::post('/subscribe', [SubscriptionController::class, 'processForm'])->name('subscribe.process');
